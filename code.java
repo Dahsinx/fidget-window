006d2298e8032b9f7f4acc060c21a756ab9e4ff9
Dahsinx <113138735+Dahsinx@users.noreply.github.com>
2022-09-08 20:25:19 +0100

    Add files via upload

---
+++ code.java
@@ -0,0 +1,27 @@
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+public class Fidget {
+    public static void main(String[] args) {
+        JButton but = new JButton("Button");
+        JFrame win = new JFrame("Fidget Window");
+        JCheckBox box = new JCheckBox("Check box");
+        JLabel sign = new JLabel("Fidget Window, a program to fidget around with buttons when you are bored, Made by Dahsinx!");
+        JFormattedTextField field = new JFormattedTextField("Text field");
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setSize(620,100);
+        win.add(but);
+        win.add(box);
+        win.add(field);
+        win.add(sign);
+        win.setVisible(true);
+        but.setSize(200, 50);
+        box.setBounds(276, 20, 125, 20);
+        field.setBounds(400,0,800,600);
+        field.setSize(200,50);
+        sign.setBounds(0, 10, 620, 100);
+    }
+}
